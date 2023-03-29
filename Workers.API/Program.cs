09e75b7216c726db4ece8ad993574516d2b5ee9c
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2023-03-29 11:50:36 +0300

    swagger & ef core added

--- Workers.API/Program.cs
+++ Workers.API/Program.cs
@@ -1,5 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using MySql.EntityFrameworkCore.Extensions;
+using Workers.DAL.Models;
+using AppContext = Workers.DAL.AppContext;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDbContext<AppContext>(options =>
+    {
+        options.UseMySQL(builder.Configuration.GetConnectionString("Workers"));
+    });
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "WorkersApi", Version = "v1" });
+});
+
 var app = builder.Build();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
 
 app.MapGet("/", () => "Hello World!");
 
