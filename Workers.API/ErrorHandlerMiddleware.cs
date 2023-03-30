4fa8b75b341291c5d53d0a7d73f8ece309b530ff
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2023-03-30 10:16:45 +0300

    add ErrorHandlerMiddleware

---
+++ Workers.API/ErrorHandlerMiddleware.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using System.Text.Json;
+
+namespace Workers.API;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(new { message = error.Message });
+            await response.WriteAsync(result);
+        }
+    }
+}
